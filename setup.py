4111904f2373c0c5a6846cf1f37dfa21cf229842
benjamin barre <benjamin.barre@gmail.com>
2022-04-19 15:32:09 +0800

    all the files online

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='Demo_Projects_CNN_numbers_recognition_Benbhk',
+setup(name='Demo_Project_CNN_numbers_recognition_Benbhk',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='Demo_Projects_CNN_numbers_recognition_Benbhk',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/Demo_Projects_CNN_numbers_recognition_Benbhk-run'],
+      scripts=['scripts/Demo_Project_CNN_numbers_recognition_Benbhk-run'],
       zip_safe=False)
