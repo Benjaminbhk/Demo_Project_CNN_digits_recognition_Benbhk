d34c390a4f80c2f37ef46aea4e88811b2e9aef7b
Benjamin Barre <benjamin.barre@gmail.com>
2023-05-01 17:22:04 +0800

    loading the pkl path correction 2

--- Demo_Project_CNN_digits_recognition_Benbhk/app.py
+++ Demo_Project_CNN_digits_recognition_Benbhk/app.py
@@ -13,8 +13,8 @@ st.image(image)
 
 # Load the model
 # model = pkl.load(open('Demo_Project_CNN_digits_recognition_Benbhk/Models/digits_recognition_model_V1.2', 'rb'))
-model = pkl.load(open('Models/digits_recognition_model_V1.2', 'rb'))
-# model = pkl.load(open('Demo_Project_CNN_digits_recognition_Benbhk/Demo_Project_CNN_digits_recognition_Benbhk/Models/digits_recognition_model_V1.2', 'rb'))
+# model = pkl.load(open('Models/digits_recognition_model_V1.2', 'rb'))
+model = pkl.load(open('Demo_Project_CNN_digits_recognition_Benbhk/Demo_Project_CNN_digits_recognition_Benbhk/Models/digits_recognition_model_V1.2', 'rb'))
 
 
 # Function to normalize image data
